70502cbbabf4023042a8f285a5725064f6e01e99
Mauro Zadunaisky <maurozadu@gmail.com>
2011-02-23 16:53:53 -0300

    Security.salt & Security.cipherSeed changed

--- config/core.php
+++ config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '4d656563e53ba_4d65656aee886_4d656570ea5df');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76859301237453540496749683159');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
