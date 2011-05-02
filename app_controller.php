3c2651d17532f46cfd29495f8031b49e8a51c2f9
Mauro Zadunaisky <maurozadu@gmail.com>
2011-05-02 15:45:17 -0300

    brwMenu demo documented

--- app_controller.php
+++ app_controller.php
@@ -5,9 +5,23 @@ class AppController extends Controller {
 	var $components = array('Session', 'Brownie.panel');
 
 	var $companyName = 'Demo app';
+
+	/**
+	* The menu should have the following structure:
+	*
+	* var $brwMenu = array(
+	* 	'Category label' => array(
+	*	 	'Button label' => array('controller' => ... ),
+	* 		'Another button' => 'ModelName',
+	* 	),
+	* 	'Another category' => array(
+	* 		etc...
+	* 	),
+	* ):
+	*/
 	var $brwMenu = array(
 		'Posts' => array(
-			//'Button Labek' => array()
+			//'Button Label' => array()
 			'Published' => array(
 				'plugin' => 'brownie', 'controller' => 'contents',
 				'action' => 'view', 'PostStatus', '2',
