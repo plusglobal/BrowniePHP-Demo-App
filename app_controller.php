97373ead3432733006f0585be9e70986b3fba83d
Mauro Zadunaisky <maurozadu@gmail.com>
2011-05-02 15:15:06 -0300

    menu config added

--- app_controller.php
+++ app_controller.php
@@ -3,6 +3,25 @@
 class AppController extends Controller {
 
 	var $components = array('Session', 'Brownie.panel');
+
 	var $companyName = 'Demo app';
+	var $brwMenu = array(
+		'Posts' => array(
+			//'Button Labek' => array()
+			'Published' => array(
+				'plugin' => 'brownie', 'controller' => 'contents',
+				'action' => 'view', 'PostStatus', '2',
+			),
+			'Draft' => array(
+				'plugin' => 'brownie', 'controller' => 'contents',
+				'action' => 'view', 'PostStatus', '1',
+			),
+		),
+		'Configuration' => array(
+			//'Button label' => 'ModelName',
+			'Cateogories' => 'Category',
+			'Tags' => 'Tag'
+		),
+	);
 
 }
